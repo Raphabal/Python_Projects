a8d9ab4bc2ce5df2faa34d549ac5bfb6f8c2eab0
Raphabal <46353401+Raphabal@users.noreply.github.com>
2024-05-08 14:39:07 -0300

    Add files via upload
    Prints an integer number and shows its predecessor and successor

---
+++ num_before_after.py
@@ -0,0 +1,5 @@
+#Solicita ao usuário informar um número
+n1 = int(input('informe um número inteiro: '))
+
+#Imprime o número informado e mostra seu antecessor e sucessor
+print('o número informado é: {}, seu sucessor: {} e antecessor: {} '.format(n1, n1+1, n1-1))
