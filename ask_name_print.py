acd8a82d1324185bf73453445df6452d0f21deaf
Raphabal <46353401+Raphabal@users.noreply.github.com>
2024-05-08 14:27:17 -0300

    Update ask_name_print.py

--- ask_name_print.py
+++ ask_name_print.py
@@ -1,2 +1,7 @@
+#Programa que solicita o nome em String e imprime Hello + nome informado
+
+#Solicita um input ao usuário
 nome = input('qual seu nome: ')
+
+#Imprime a palavra "Hello" com o nome informado
 print('hello, {}!'.format(nome))
