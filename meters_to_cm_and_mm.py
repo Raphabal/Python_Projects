e3a077ef7e7ad0ad7a204b8f7e49a70383533044
Raphabal <46353401+Raphabal@users.noreply.github.com>
2024-07-01 16:21:32 -0300

    Add files via upload
    Convert meters to centimeters and millimeters

---
+++ meters_to_cm_and_mm.py
@@ -0,0 +1,11 @@
+#Transformando metros em centimetros e milimetros
+m = float(input('insita a metragem em metros '))
+
+#Calculo de Centimetros
+c = m * 100
+
+#Calcualo de milimetros
+mm = c * 10
+
+#Mostra resultados
+print('o valor de m em cm é: {} e em mm é: {}'.format(c, mm))
