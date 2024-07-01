18f97474cced778f6b51982232dbcffbf46312a2
Raphabal <46353401+Raphabal@users.noreply.github.com>
2024-07-01 16:15:47 -0300

    Update and rename double_triple_sqroot.py to mean_grades.py

--- double_triple_sqroot.py
+++
@@ -1,5 +0,0 @@
-#ask for an integer number 
-n1 = int(input('insira um numero inteiro: '))
-
-#Prints the double, triple and square root of the inputed number
-print('o dobro de {} é {}, \ntriplo {} \ne raiz quadrada {:.2f}'.format(n1, 2*n1, 3*n1, n1**(1/2)))
---
+++ mean_grades.py
@@ -0,0 +1,11 @@
+#Insira a nota da prova 1
+nota1 = float(input('informe a nota da prova 1 '))
+
+#Insira a nota da prova 2
+nota2 = float(input('informe a nota da prova 2 '))
+
+#Calculo da média
+media = (nota1 + nota2)/2
+
+#Mostra o resultado da média das notas
+print('a media do aluno é {}'.format(media))    
