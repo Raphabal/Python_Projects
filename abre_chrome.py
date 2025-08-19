781972593fc6d3ee5657fe52d784fe7c23accfa4
Raphabal <46353401+Raphabal@users.noreply.github.com>
2025-08-19 09:04:37 -0300

    Add files via upload

---
+++ abre_chrome.py
@@ -0,0 +1,10 @@
+def main():
+    import pyautogui
+    pyautogui.press("win")
+    pyautogui.write('chrome')
+    pyautogui.press("enter")
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+if __name__ == "__main__":
+    main()
